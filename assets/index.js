ab48c94573be15f840442c2b95ee1e751986c542
skyujilong <skyujilong@163.com>
2018-11-06 18:03:11 +0800

    更改调用native app的方式。排除uc的调用

--- assets/index.js
+++ assets/index.js
@@ -84,7 +84,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 var ua = navigator.userAgent.toLowerCase();
 var isChrome = /(chrome|crios)\/([\d.]*)/.test(ua);
-var isBaidu = /baidu/i.test(ua);
+// var isBaidu = /baidu/i.test(ua);
 var ios8up = /version\/(\d+)(:?.+)mobile(:?.+)safari(:?.+)$/i.test(ua) && RegExp.$1 > 8;
 var isUC = /UCBrowser/i.test(ua);
 var isQQ = /MQQBrowser/i.test(ua);
@@ -96,12 +96,15 @@ var isRuning = false;
 var isWX = /MicroMessenger/i.test(ua);
 var defalutDelayTime = 1.5 * 1000;
 var errorIde; //普通错误setTimeout id
-var openIde; //window.open的setInterval id
 /**
  * 失败异常处理
  */
 function errorCbHandler(errorCb, delayTime) {
-    if (isSafari || (isUC && isIos)) {
+    //uc现在打不开了
+    if(isUC){
+        isRuning = false;
+        errorCb();
+    }else if (isSafari) {
         //ios8 以上后台setInterval 确定会被 滞后执行，
         //但是android因为是多线程的所以貌似不行的  uc浏览器也有同样的问题
         //android 浏览器 测试uc
@@ -152,7 +155,7 @@ module.exports = {
         if (Object.prototype.toString.call(errorCb) !== '[object Function]') {
             throw new Error('errorCb must be a function!');
         }
-        if(isWX){
+        if (isWX) {
             var code = '';
             code = isIos ? 'ios' : 'android';
             wxCb(code);
@@ -180,20 +183,17 @@ module.exports = {
             document.addEventListener('-webkit-visibilitychange', changeVisibility, false);
         }
 
-        if ((isChrome && !isBaidu && !isQQ) || ios8up ) {
-            var openWin = window.open(url, "_self", "height=1,width=1,top=0,left=0,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no");
-            openIde = setInterval(function() {
-                if ('object' == typeof openWin) {
-                    clearInterval(openIde);
-                    openWin.close();
-                }
-            }, 10);
-        } else {
-            var iframe = document.createElement('iframe');
-            iframe.style.display = 'none';
-            iframe.src = url;
-            document.body.appendChild(iframe);
+        let iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        iframe.style.height='0';
+        iframe.style.overflow='hidden';
+        iframe.frameborder='none';
+        try{
+            iframe.src = `javascript:document.write("<html><head></head><body><script>location.href='${url}';</script></body></html>")`;
+        }catch(e){
+            
         }
+        document.body.appendChild(iframe);
     }
 }
 
